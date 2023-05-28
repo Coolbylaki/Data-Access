f5515d2e43890e3bc58d5e4c769f77fb9dd039b4
Lazar Stojanović <lakisd98@gmail.com>
2023-05-28 02:20:31 +0200

    Add: button styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import Button from "./components/UI/Button";
+import Questions from "./components/Questions";
+import { useState } from "react";
+
 function App() {
-	return <h1>Test</h1>;
+	const [showQuestions, setShowQuestions] = useState(false);
+
+	return (
+		<>
+			{!showQuestions && (
+				<Button
+					onClick={() => {
+						setShowQuestions(true);
+					}}
+					className="rounded border-[3px] border-[#d3d3da] bg-[#d3d3da] p-3 text-2xl font-bold hover:bg-black hover:text-white">
+					Click to start quiz
+				</Button>
+			)}
+			{showQuestions && <Questions />}
+		</>
+	);
 }
 
 export default App;
