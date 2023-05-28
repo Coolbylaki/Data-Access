1c2e139ee3ce6505366bf05d30d091a94ca431ef
Lazar Stojanović <lakisd98@gmail.com>
2023-05-28 09:50:39 +0200

    Fix: footer not sticking to bottom

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
-		<footer className="hidden text-center text-white sm:flex">
-			<p className="absolute bottom-0 right-0">
+		<footer className="hidden text-white sm:flex">
+			<p className="fixed bottom-0 right-0 mr-1">
 				Made by:<span className="text-xl font-[500]"> Lazar Stojanović</span>
 			</p>
 		</footer>
